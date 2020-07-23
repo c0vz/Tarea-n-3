dc221e15c900db8a013846508a410ebdc8031fbb
c0vz <42586145+c0vz@users.noreply.github.com>
2020-07-23 00:32:52 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser= require('body-parser');
-
+const port = process.env.PORT || 3000;
 // para parsear parametros a json.
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
